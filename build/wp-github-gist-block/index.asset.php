5209877feb70e57824094ca755ac0199b351b2ca
Missie Dawes <missiedawes@gmail.com>
2025-06-21 20:58:30 +0000

    Choose higlight.js style

--- build/wp-github-gist-block/index.asset.php
+++ build/wp-github-gist-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4cca8648f8581fbdd8ad');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3aa3aa0d5532de148351');
