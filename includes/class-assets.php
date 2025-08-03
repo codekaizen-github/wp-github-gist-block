6c70f1c7b1a95531f89c74b737cd819ef0fb4b4f
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-08-03 19:22:52 +0000

    Only enqueue if block is present

--- includes/class-assets.php
+++ includes/class-assets.php
@@ -26,10 +26,49 @@ class WP_Github_Gist_Block_Assets
     }
 
     /**
+     * Check if current post contains the GitHub Gist block
+     *
+     * @return bool Whether the current post contains a GitHub Gist block
+     */
+    private function has_gist_block()
+    {
+        // Always load in the admin/editor
+        if (is_admin()) {
+            return true;
+        }
+
+        // Check if we're on a singular page
+        if (!is_singular()) {
+            return false;
+        }
+
+        // Get the current post
+        $post = get_post();
+        if (!$post) {
+            return false;
+        }
+
+        // Look for the block in the content
+        $block_name = 'create-block/wp-github-gist-block';
+
+        // Check if the post uses the block editor
+        if (use_block_editor_for_post($post)) {
+            return has_block($block_name, $post->post_content);
+        }
+
+        return false;
+    }
+
+    /**
      * Enqueue styles
      */
     public function enqueue_styles()
     {
+        // Only load if we have the block in the post
+        if (!$this->has_gist_block()) {
+            return;
+        }
+
         // Get settings
         $settings = WP_Github_Gist_Block_Admin_Settings::get_settings();
 
